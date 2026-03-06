68c4ac35ce9d5edd192c822b4d94d3ce281bea79
Stoff73 <c.jones@csjones.co>
2026-03-06 08:41:32 +0000

    Initial Commit

---
+++ FastingTrackerTests/FastingTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  FastingTrackerTests.swift
+//  FastingTrackerTests
+//
+//  Created by ChrisSlater-Jones on 06/03/2026.
+//
+
+import Testing
+
+struct FastingTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
